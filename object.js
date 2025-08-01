267275e75a99d59a368085659f50e4c821d6126b
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-08-01 15:26:38 +0600

    introduction to object

---
+++ object.js
@@ -0,0 +1,28 @@
+// primitive , basic or the lowest level
+let age = 21;
+let university = 'Daffodil International University';
+let isPassed = true;
+let isDeveloper;
+let Subject =['Math', 'Physics', 'Chemsitry', 'Biology', 'Higher Math'];
+console.log(isDeveloper);
+
+// let bottle =['white', 45] real life things can not be explained with simple variable like strig or array 
+//For the things of the real life the most and one of  the interesting thing comes that is OBJECT
+
+// Object Declearing 
+//Obejct is known as key value property means the key and value comes in twin situation
+//Object is a non-primitive data type
+let bottle ={
+    // property : value 
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+};
+
+let subject = {
+    name : 'bioogy',
+    tutor : 'sakib',
+    chapter: ['First', 'Seconds', 'Thirs'],
+    examDate : '6 aug 2025'
+};
